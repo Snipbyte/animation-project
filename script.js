67bd6ec9539dbb93e57986d67a3f8a7f236fa801
kashifx6 <Tabkashifx2>
2024-04-01 20:53:47 +0500

    some changes

---
+++ script.js
@@ -0,0 +1,21 @@
+gsap.to(".box", {
+  y: 1000,
+
+  backgroundColor: "red",
+  duration: 2,
+});
+
+
+gsap.to(".project-box", {
+  y: -1000,
+ 
+  backgroundColor: "red",
+  duration: 2,
+});
+
+gsap.to(".wrapper",{
+  duration:5,
+  opacity:1
+})
+
+
